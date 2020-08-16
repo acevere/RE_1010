31a214d37d2815ffc44bc8707c761151190a6170
acevere <assver.ay@gmail.com>
2020-08-16 17:01:14 -0400

    Initial Commit

---
+++ RE_1010/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RE_1010
+//
+//  Created by Alice Ye on 2020-08-16.
+//  Copyright © 2020 Alice Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
